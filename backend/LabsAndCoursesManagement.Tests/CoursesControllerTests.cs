c6a446941cb445b608ef00465a589364d9506344
Samuel Chiscari <79214849+ChiscariSamuelA3@users.noreply.github.com>
2022-11-25 15:00:30 +0200

    Added Test for Post Course API

--- backend/LabsAndCoursesManagement.Tests/CoursesControllerTests.cs
+++ backend/LabsAndCoursesManagement.Tests/CoursesControllerTests.cs
@@ -1,5 +1,6 @@
 ﻿using LabsAndCoursesManagement.Domain;
 using Newtonsoft.Json;
+using System.Text;
 
 namespace LabsAndCoursesManagement.Tests
 {
@@ -9,7 +10,7 @@ namespace LabsAndCoursesManagement.Tests
         [TestMethod]
         public async Task Get_CheckIfReturnsPostedCourse()
         {
-            _db.Courses.Add(new Course("TestCourse", 1, 5));
+            _db.Courses.Add(new Course("TestCourseGet", 1, 5));
             _db.SaveChanges();
 
 
@@ -17,7 +18,24 @@ namespace LabsAndCoursesManagement.Tests
             var content = await response.Content.ReadAsStringAsync();
             Console.WriteLine(content);
             var courses = JsonConvert.DeserializeObject<List<Course>>(content);
-            Assert.IsTrue(courses.Any(c => c.Title == "TestCourse"));
+            Assert.IsTrue(courses.Any(c => c.Title == "TestCourseGet"));
+
+            // ensure deleted
+            _db.Courses.RemoveRange(_db.Courses);
+        }
+
+        [TestMethod]
+        public async Task Post_CheckIfReturnsPostedCourse()
+        {
+            var course = new Course("TestCoursePost", 1, 5);
+            var content = JsonConvert.SerializeObject(course);
+            var stringContent = new StringContent(content, Encoding.UTF8, "application/json");
+            
+            var response = await _httpClient.PostAsync("api/courses", stringContent);
+            var responseContent = await response.Content.ReadAsStringAsync();
+            var postedCourse = JsonConvert.DeserializeObject<Course>(responseContent);
+            
+            Assert.AreEqual(course.Title, postedCourse.Title);
 
             // ensure deleted
             _db.Courses.RemoveRange(_db.Courses);
