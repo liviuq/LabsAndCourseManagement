21a0c9d77876e1252c2c5b1acf77387980e9d28c
Crekto <hiimcrekto@yahoo.com>
2022-12-08 21:24:03 +0200

    Sidebar+routes

--- frontend/src/shared/Header.tsx
+++ frontend/src/shared/Header.tsx
@@ -1,21 +1,44 @@
-import React from 'react'
-import { Avatar, Button, Flex, Menu, MenuButton, MenuItem, MenuList, Text } from '@chakra-ui/react'
-import { GiBookmarklet } from 'react-icons/gi'
-import { LangChanger } from './LangChanger'
-
+import React from "react";
+import {
+  Avatar,
+  Button,
+  Flex,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  Text,
+} from "@chakra-ui/react";
+import { GiBookmarklet } from "react-icons/gi";
+import { LangChanger } from "./LangChanger";
+import { useNavigate } from "react-router-dom";
 
 export const Header: React.FC = () => {
+  const navigate = useNavigate();
   return (
-    <Flex h="6vh" w="full" align="center" px="32px" justify="space-between" bg="white" shadow="0px 2px 8px rgba(0, 0, 0, 0.2)" zIndex="10">
-      <GiBookmarklet size="4vh" color='#00ABB3' cursor="pointer" />
+    <Flex
+      h="6vh"
+      w="full"
+      align="center"
+      px="32px"
+      justify="space-between"
+      bg="white"
+      shadow="0px 2px 8px rgba(0, 0, 0, 0.2)"
+      zIndex="10"
+    >
+      <GiBookmarklet
+        onClick={() => navigate("/")}
+        size="4vh"
+        color="#00ABB3"
+        cursor="pointer"
+      />
       <Flex align="center" gap="32px">
-
         <Flex align="center" gap="12px">
-          <Text fontSize="lg">Achim Stefan</Text>
+          <Text fontSize="lg">Administrator</Text>
           <Avatar boxSize="32px" bg="#00ABB3" />
         </Flex>
         <LangChanger />
       </Flex>
-    </Flex >
-  )
-}
+    </Flex>
+  );
+};
--- frontend/src/shared/Sidebar.tsx
+++ frontend/src/shared/Sidebar.tsx
@@ -1,10 +1,27 @@
-import { Flex, Text } from '@chakra-ui/react'
-import React from 'react'
+import { Button, Flex, Text } from "@chakra-ui/react";
+import { useNavigate } from "react-router-dom";
+import React from "react";
 
 export const Sidebar: React.FC = () => {
+  const navigate = useNavigate();
   return (
-    <Flex h="94vh" w="15%" minW="300px" bg="#EAEAEA">
-      <Text></Text>
+    <Flex h="94vh" w="15%" minW="300px" bg="#EAEAEA" direction="column">
+      <Button
+        onClick={() => navigate("/teacher")}
+        variant="solid"
+        colorScheme="teal"
+        borderRadius="0"
+      >
+        Teachers
+      </Button>
+      <Button
+        onClick={() => navigate("/student")}
+        variant="solid"
+        colorScheme="teal"
+        borderRadius="0"
+      >
+        Students
+      </Button>
     </Flex>
-  )
-}
+  );
+};
