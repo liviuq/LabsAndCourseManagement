281eed705f83931fc868312234dabeafa0d87da5
Samuel Chiscari <79214849+ChiscariSamuelA3@users.noreply.github.com>
2022-12-16 02:25:14 +0200

    added 2 more integration tests for StudentsController

--- backend/LabsAndCoursesManagement.Tests/StudentsControllerTests.cs
+++ backend/LabsAndCoursesManagement.Tests/StudentsControllerTests.cs
@@ -1,4 +1,5 @@
-﻿using LabsAndCoursesManagement.Domain;
+﻿using System.Text;
+using LabsAndCoursesManagement.Domain;
 using Newtonsoft.Json;
 
 namespace LabsAndCoursesManagement.Tests
@@ -20,6 +21,43 @@ namespace LabsAndCoursesManagement.Tests
 
             Assert.IsTrue(students != null);
             Assert.IsTrue(students.Any(s => s.Email == "mockemail"));
+
+            // ensure deleted
+            _db.Students.RemoveRange(_db.Students);
+        }
+
+        [TestMethod]
+        public async Task StudentController_GetById_ReturnsEmptyResponse()
+        {
+            _db.Students.Add(new Student("mockemail", "mockName", "mockLastName", 3, "2B4", 500));
+            _db.SaveChanges();
+
+            var student = _db.Students.FirstOrDefault(s => s.Email == "mockemail");
+
+            var response = await _httpClient.GetAsync($"api/Students/{student.Id}");
+            var content = await response.Content.ReadAsStringAsync();
+
+            var studentFromResponse = JsonConvert.DeserializeObject<Student>(content);
+
+            Assert.IsTrue(studentFromResponse != null);
+            Assert.IsTrue(studentFromResponse.Email == "mockemail");
+
+            // ensure deleted
+            _db.Students.RemoveRange(_db.Students);
+        }
+
+        [TestMethod]
+        public async Task StudentController_Create_ReturnsEmptyResponse()
+        {
+            var student = new Student("mockemail", "mockName", "mockLastName", 3, "2B4", 500);
+
+            var response = await _httpClient.PostAsync("api/Students", new StringContent(JsonConvert.SerializeObject(student), Encoding.UTF8, "application/json"));
+            var content = await response.Content.ReadAsStringAsync();
+
+            var studentFromResponse = JsonConvert.DeserializeObject<Student>(content);
+
+            Assert.IsTrue(studentFromResponse != null);
+            Assert.IsTrue(studentFromResponse.Email == "mockemail");
 
             // ensure deleted
             _db.Students.RemoveRange(_db.Students);
